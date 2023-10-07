12ac89e37af7feddd49fe985a0d0a26c664b3d0a
Kozyriev <S.Kozurev@ukr.net>
2023-10-07 16:19:59 +0300

    add

--- src/main/java/org/example/ExceptionsPractice.java
+++ src/main/java/org/example/ExceptionsPractice.java
@@ -1,2 +1,37 @@
-package org.example;public class ExeptionsPractice {
+package org.example;
+
+import java.sql.Time;
+
+public class ExceptionsPractice {
+    public static void main(String[] args) {
+        pause(300);
+        arithmeticException(11, 0);
+        
+    }
+
+    private static void arithmeticException(int divident, int diveder) {
+        try {
+            int res = divident/diveder;
+        }catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+            System.out.println(e.getLocalizedMessage());
+            System.out.println(e.getClass());
+        }finally {
+            System.out.println("finnaly");
+        }
+
+    }
+
+    private static void pause(int time) {
+
+        try {
+            Thread.sleep(300);
+        }catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }finally {
+            System.out.println("finnaly");
+        }
+
+
+    }
 }
